de2a3bec9058dfddd935380180818f9f6e72526f
Nazifah13 <23004948@siswa.um.edu.my>
2024-12-29 16:37:21 +0800

    Added Legal Doc Template

--- app/src/main/java/com/example/rightside/ModifyLegalConsultation.java
+++ app/src/main/java/com/example/rightside/ModifyLegalConsultation.java
@@ -9,29 +9,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import java.util.ArrayList;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DisplayRequestPage#newInstance} factory method to
+ * Use the {@link ModifyLegalConsultation#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DisplayRequestPage extends Fragment {
+public class ModifyLegalConsultation extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private static final ArrayList<Request> requests = new ArrayList<>();
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public DisplayRequestPage() {
+    public ModifyLegalConsultation() {
         // Required empty public constructor
     }
 
@@ -41,11 +38,11 @@ public class DisplayRequestPage extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_display_request.
+     * @return A new instance of fragment fragment_modify_consultation.
      */
     // TODO: Rename and change types and number of parameters
-    public static DisplayRequestPage newInstance(String param1, String param2) {
-        DisplayRequestPage fragment = new DisplayRequestPage();
+    public static ModifyLegalConsultation newInstance(String param1, String param2) {
+        ModifyLegalConsultation fragment = new ModifyLegalConsultation();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -66,34 +63,20 @@ public class DisplayRequestPage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_display_request, container, false);
+        return inflater.inflate(R.layout.fragment_modify_legal_consultation, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        LinearLayout cardContainer = view.findViewById(R.id.ViewRequestCardContainer);
-        for (int i = 0; i < 20; i++) {
-            requests.add(new Request("Tajuk", "Information and Guidance", "Completed"));
-        }
+        Spinner spinnerReasonConsultation = view.findViewById(R.id.LegalConsultationReasonSpinner);
+        ArrayAdapter<CharSequence> reasonConsultationAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.LegalConsultationReason, R.layout.layout_spinner);
+        spinnerReasonConsultation.setAdapter(reasonConsultationAdapter);
 
-        for(int i=0 ; i<requests.size() ; i++){
-            addCard(cardContainer,requests.get(i));
-        }
-    }
+        Spinner spinnerDesiredOutcome = view.findViewById(R.id.LegalDesiredOutcomeSpinner);
+        ArrayAdapter<CharSequence> desiredOutcomeAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.LegalDesiredOutcome, R.layout.layout_spinner);
+        spinnerDesiredOutcome.setAdapter(desiredOutcomeAdapter);
 
-    private void addCard(LinearLayout container, Request request){
-        View card = LayoutInflater.from(getActivity()).inflate(R.layout.card_view_request,container,false);
-
-        TextView cardTitle = card.findViewById(R.id.ButtonViewRequest);
-        TextView cardDesiredOutcome = card.findViewById(R.id.ViewRequestDesiredOutcome);
-        TextView cardUpdateStates = card.findViewById(R.id.ViewRequestStatusUpdate);
-
-        cardTitle.setText(request.title);
-        cardDesiredOutcome.setText(request.desiredOutcome);
-        cardUpdateStates.setText(request.status);
-
-        container.addView(card);
     }
 }
--- app/src/main/java/com/example/rightside/ModifyMentalConsultation.java
+++ app/src/main/java/com/example/rightside/ModifyMentalConsultation.java
@@ -2,18 +2,22 @@ package com.example.rightside;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ModifyConsultation#newInstance} factory method to
+ * Use the {@link ModifyMentalConsultation#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ModifyConsultation extends Fragment {
+public class ModifyMentalConsultation extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +28,7 @@ public class ModifyConsultation extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ModifyConsultation() {
+    public ModifyMentalConsultation() {
         // Required empty public constructor
     }
 
@@ -34,11 +38,11 @@ public class ModifyConsultation extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_modify_consultation.
+     * @return A new instance of fragment fragment_modify_mental_consultation.
      */
     // TODO: Rename and change types and number of parameters
-    public static ModifyConsultation newInstance(String param1, String param2) {
-        ModifyConsultation fragment = new ModifyConsultation();
+    public static ModifyMentalConsultation newInstance(String param1, String param2) {
+        ModifyMentalConsultation fragment = new ModifyMentalConsultation();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +63,20 @@ public class ModifyConsultation extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_modify_legal_consultation, container, false);
+        return inflater.inflate(R.layout.fragment_modify_mental_consultation, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Spinner spinnerReasonConsultation = view.findViewById(R.id.MentalConsultationReasonSpinner);
+        ArrayAdapter<CharSequence> reasonConsultationAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.MentalConsultationReason, R.layout.layout_spinner);
+        spinnerReasonConsultation.setAdapter(reasonConsultationAdapter);
+
+        Spinner spinnerDesiredOutcome = view.findViewById(R.id.MentalDesiredOutcomeSpinner);
+        ArrayAdapter<CharSequence> desiredOutcomeAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.MentalDesiredOutcome, R.layout.layout_spinner);
+        spinnerDesiredOutcome.setAdapter(desiredOutcomeAdapter);
+
     }
 }
